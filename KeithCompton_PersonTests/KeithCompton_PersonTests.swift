01caec9288e00b2c6fd72df986249cd0389e63b7
kac2z7 <keith@temps-macbook-pro-2.local>
2017-09-18 18:59:08 -0500

    Initial Commit

---
+++ KeithCompton_PersonTests/KeithCompton_PersonTests.swift
@@ -0,0 +1,36 @@
+//
+//  KeithCompton_PersonTests.swift
+//  KeithCompton_PersonTests
+//
+//  Created by Keith on 9/18/17.
+//  Copyright © 2017 Keith. All rights reserved.
+//
+
+import XCTest
+@testable import KeithCompton_Person
+
+class KeithCompton_PersonTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
